334eaa8a760803fd631305d7c0ea1acbe7e1e913
Sqmuel1 <143673257+Sqmuel1@users.noreply.github.com>
2024-08-27 21:54:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./img/encriptado.png";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+}
+  
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+  
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.png";
+      } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+      }
+}
+function copiarTexto() {
+    const textoEncriptado = document.getElementById("texto").value;
+    
+    if (textoEncriptado.length !== 0) {
+        navigator.clipboard.writeText(textoEncriptado).then(() => {
+            swal("¡Éxito!", "Texto copiado al portapapeles", "success");
+        }).catch(err => {
+            swal("Ooops!", "No se pudo copiar el texto", "error");
+        });
+    } else {
+        swal("Ooops!", "No hay texto para copiar", "warning");
+    }
+}
